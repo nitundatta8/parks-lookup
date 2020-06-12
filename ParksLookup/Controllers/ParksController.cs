3cd42bf93f5ac1bdfa64de340b0714b813435e69
nitunwa <nitun.2@gmail.com>
2020-06-12 12:04:36 -0700

    add edit and delete functinality to the parkcontroller and successfully working all CRUD operations

--- ParksLookup/Controllers/ParksController.cs
+++ ParksLookup/Controllers/ParksController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ParksLookup.Models;
 
 namespace ParksLookup.Controllers
@@ -25,6 +26,31 @@ namespace ParksLookup.Controllers
     public void Post([FromBody] Park park)
     {
       _db.Parks.Add(park);
+      _db.SaveChanges();
+    }
+
+    // GET api/parks/3
+    [HttpGet("{id}")]
+    public ActionResult<Park> Get(int id)
+    {
+      return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+    }
+
+    // PUT api/parks/3
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Park park)
+    {
+      park.ParkId = id;
+      _db.Entry(park).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/parks/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+      _db.Parks.Remove(parkToDelete);
       _db.SaveChanges();
     }
   }
